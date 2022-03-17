94b8ccc414980ca1c5a85a73ce1bf636f6af2e72
unknown <julienduval17@gmail.com>
2022-03-17 16:26:12 +0100

    graph

---
+++ readme.txt
@@ -0,0 +1,23 @@
+<--------   Technologies de programmation utilisées   --------->
+
+Pour réaliser ce projet, les technologies utilisées sont : HTML, Bootstrap, PHP, Javascript, JQuery, Ajax, Mysql
+
+
+<--------   Connexion à la base de données   --------->
+
+Les tests se font en Local.
+Le fichier de connexion à la base de données se trouve dans le dossier "treatment".
+La base de données exportée se trouve dans le dossier "bdd".
+
+
+<--------   Explication brève du projet   --------->
+
+La page d'accueil regroupe tous les modules. Au clic sur la simulation, elle se lance et toutes les secondes, des données sont 
+envoyées et enregistrés dans la base de données.
+Si un module surchauffe, une alerte s'affiche et le module s'arrête.
+
+Le dashbard permet de voir à l'aide d'un graphique l'évolution des valeurs mesurées durant les simulations.
+
+<--------   Autre source   --------->
+
+Lien vers le GitHub : https://github.com/Julien1705/monitoring__iot
--- treatment/requeteSql/addModule.php
+++ treatment/requeteSql/addModule.php
@@ -6,31 +6,30 @@ require "../connexionBdd.php";
 
 <?php
 
-// Vérification que tous les éléments du formulaire sont présents
-if (!empty($_POST["name"]) && !empty($_POST["description"]) && !empty($_POST["current_measured_value"]) && !empty($_POST["operating_time"]) && !empty($_POST["number_of_data_sent"]) && !empty($_POST["temperature"]) && !empty($_POST["state"])) {
 
-    // Stockage des éléments du module
-    $name = $_POST["name"];
-    $description = $_POST["description"];
-    $current_measured_value = $_POST["current_measured_value"];
-    $operating_time = $_POST["operating_time"];
-    $number_of_data_sent = $_POST["number_of_data_sent"];
-    $temperature = $_POST["temperature"];
-    $state = $_POST["state"];
-    
+// Stockage des éléments du module
+$name = $_POST["name"];
+$description = $_POST["description"];
+$current_measured_value = $_POST["current_measured_value"];
+$operating_time = $_POST["operating_time"];
+$number_of_data_sent = $_POST["number_of_data_sent"];
+$temperature = $_POST["temperature"];
+$state = $_POST["state"];
 
-    // Insertion des éléments dans la base de données
-    $sql = "INSERT INTO `module`(`name`, `description`, `current_measured_value`, `operating_time`, `number_of_data_sent`, `temperature`, `state`) VALUES (:name,:description,:current_measured_value,:operating_time,:number_of_data_sent,:temperature,:state)";
-    $req = $db->prepare($sql);
-    $req->execute([
-        "name" => $name,
-        "description" => $description,
-        "current_measured_value" => $current_measured_value,
-        "operating_time" => $operating_time,
-        "number_of_data_sent" => $number_of_data_sent,
-        "temperature" => $temperature,
-        "state" => $state
-    ]);
+echo ($temperature);
 
-    header('location: ../../index.php');
-}
+
+// Insertion des éléments dans la base de données
+$sql = "INSERT INTO `module`(`name`, `description`, `current_measured_value`, `operating_time`, `number_of_data_sent`, `temperature`, `state`) VALUES (:name , :description , :current_measured_value , :operating_time, :number_of_data_sent, :temperature, :state)";
+$req = $db->prepare($sql);
+$req->execute([
+    "name" => $name,
+    "description" => $description,
+    "current_measured_value" => $current_measured_value,
+    "operating_time" => $operating_time,
+    "number_of_data_sent" => $number_of_data_sent,
+    "temperature" => $temperature,
+    "state" => $state
+]);
+
+header('location: ../../index.php');
